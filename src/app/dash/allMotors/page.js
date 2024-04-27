b793eaa3f5bcc5a7b02f847a1040b67bc277ed0f
Shashank <shashank.shri10@gmail.com>
2024-04-27 16:34:44 +0530

    some more

--- src/app/dash/allMotors/page.js
+++ src/app/dash/allMotors/page.js
@@ -84,11 +84,11 @@ useEffect(() => {
                 submissions.map((submission) => (
                   <tr
                     key={submission.id}
-                    className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+                    className="odd:bg-white even:bg-gray-50 border-b "
                   >
                     <th
                       scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
                     >
                       {submission.motor_id}
                     </th>
@@ -100,7 +100,7 @@ useEffect(() => {
                     <td className="px-6 py-4">
                       <Link
                         href={`./motor/${submission.motor_id}/`}
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                        className="font-medium text-blue-600 hover:underline"
                         onClick={() =>
                           router.push(`./motor/${submission.motor_id}/`)
                         }
